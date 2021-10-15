1378aba74230846bd6f22d711e2b58654d27caf2
emre-yunus <r0664998@student.thomasmore.be>
2021-10-15 10:53:43 +0200

    Made test for Aged Bie

---
+++ src/test/java/com/gildedrose/AgedBrieTest.java
@@ -0,0 +1,58 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AgedBrieTest {
+
+    @Test
+    void AgedBrieTestS5Q0() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(1, app.items[0].quality);
+    }
+
+    @Test
+    void AgedBrieTestS0Q0() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(2, app.items[0].quality);
+    }
+
+    @Test
+    void AgedBrieTestS3Q50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 3, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(2, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    void AgedBrieTestS0Q49() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 49) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    void AgedBrieTestNameDifferent() {
+        Item[] items = new Item[] { new Item("AgedBrie", 5, 2) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("AgedBrie", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(1, app.items[0].quality);
+    }
+}
